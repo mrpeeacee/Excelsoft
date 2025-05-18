90e5ccccdd061292bcf9d1588972a8743bbe5518
Nikhil <113583720+mrpeeacee@users.noreply.github.com>
2025-05-18 08:55:36 +0000

    update

---
+++ AWS/Teli_Udemy/Terraform/Execise2/Instance.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.amiID.id
+  instance_type          = "t3.micro"
+  key_name               = "dove-key"
+  vpc_security_group_ids = [aws_security_group.dove-sg.id]
+  subnet_id              = "subnet-029f2275"
+  availability_zone      = "us-east-1a"
+
+  tags = {
+    Name    = "Dove-web"
+    Project = "Dove"
+  }
+}
+
+resource "aws_ec2_instance_state" "web-state" {
+  instance_id = aws_instance.web.id
+  state       = "running"
+}
---
+++ AWS/Teli_Udemy/Terraform/Execise2/Keypair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "dove-key" {
+  key_name   = "dove-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMrY1JbsXyHcEgssa7iyD30FJbmI6R9Ag1lT78Bzf8OZ nikhil.mg@ES-LAPTOP-575"
+}
